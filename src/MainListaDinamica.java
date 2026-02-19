b9c5df8686d67936a7b6d35b3a6a17fc66530588
ricardo <ricardo.petri@unifil.br>
2026-02-19 20:25:19 -0300

    Implementação Lista Dinâmica

---
+++ src/MainListaDinamica.java
@@ -0,0 +1,12 @@
+public class MainListaDinamica {
+    public static void main(String[] args) {
+        ListaDinamica listaDinamica = new ListaDinamica();
+        listaDinamica.adicionarElemento("Arroz");
+        listaDinamica.adicionarElemento("Feijão");
+        listaDinamica.adicionarElemento("Feijão");
+        listaDinamica.adicionarElemento("Feijão");
+        listaDinamica.adicionarElemento("Feijão");
+        listaDinamica.adicionarElemento("Feijão");
+        listaDinamica.exibirElementos();
+    }
+}
