eefc02aa613c48d6a7f876e1cf0eda8322b556ef
ricardo <ricardo.petri@unifil.br>
2026-03-02 22:19:25 -0300

    Implementação Fila Simples

--- src/ListaSimples.java
+++ src/ListaSimples.java
@@ -1,4 +1,4 @@
-public class ListaSimples {
+public class ListaSimples implements ListaOperacoes{
     String[] lista;
 
     public ListaSimples(int tamanho) {
@@ -88,5 +88,55 @@ public class ListaSimples {
             }
         }
         System.out.println("A lista possui " + cont + " elementos!");
+    }
+
+    @Override
+    public int removerTodas(String elemento) {
+        return 0;
+    }
+
+    @Override
+    public int contar() {
+        return 0;
+    }
+
+    @Override
+    public int adicionarVarios(String[] elementos) {
+        return 0;
+    }
+
+    @Override
+    public String obter(int indice) {
+        return "";
+    }
+
+    @Override
+    public boolean inserir(int indice, String elemento) {
+        return false;
+    }
+
+    @Override
+    public String removerPorIndice(int indice) {
+        return "";
+    }
+
+    @Override
+    public void limpar() {
+
+    }
+
+    @Override
+    public int ultimoIndiceDe(String elemento) {
+        return 0;
+    }
+
+    @Override
+    public int contarOcorrencias(String elemento) {
+        return 0;
+    }
+
+    @Override
+    public int substituir(String antigo, String novo) {
+        return 0;
     }
 }
---
+++ src/MainFilaSimples.java
@@ -0,0 +1,19 @@
+public class MainFilaSimples {
+    public static void main(String[] args) {
+        FilaSimples filaSimples = new FilaSimples(10);
+        filaSimples.exibirFila();
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.enfileirar(1);
+        filaSimples.exibirFila();
+    }
+}
